3559f9d05b854666fe57e749a79eddab5a6f880f
jaymes15 <jamesoluwatomisinomotosho@gmail.com>
2020-07-24 07:02:07 +0400

    confirm bank details

--- paystacktransapp/urls.py
+++ paystacktransapp/urls.py
@@ -7,5 +7,6 @@ from django.urls import path
 app_name= 'paystacktransapp'
 urlpatterns = [
 	path('',views.homepage, name='homepage'), 
+	path('checkaccount/',views.checkaccount,name="checkaccount")
 	
 	]
--- paystacktransapp/views.py
+++ paystacktransapp/views.py
@@ -15,7 +15,35 @@ def homepage(request):
 	response = requests.get('https://api.paystack.co/bank', headers={
     				'Authorization': 'Bearer sk_test_1590017dabc1772e81fd0e246379f6e6ebcfac80',
                   })
-	if response.status_code == 200:
-		result = response.json()
-		context = {'result':result}
-	return render(request,'paystacktransapp/homepage.html',context)
+	try:
+		if response.status_code == 200:
+			results = response.json()
+			result = results['data']
+			context = {'result':result}
+			return render(request,'paystacktransapp/homepage.html',context)
+	except Exception as ex:
+		context = {"error":ex}
+		return render(request,'paystacktransapp/homepage.html',context)
+
+
+def checkaccount(request):
+	try:
+		print("hello")
+		bankcode = request.POST.get('bankcode',None)
+		accountnumber = request.POST.get('accountnumber',None)
+		headers = {
+    				'Authorization: Bearer sk_test_1590017dabc1772e81fd0e246379f6e6ebcfac80',
+                  }
+		response = requests.get('https://api.paystack.co/bank/resolve?account_number='+accountnumber+'&bank_code='+bankcode, headers={
+        			      'Authorization': 'Bearer sk_test_1590017dabc1772e81fd0e246379f6e6ebcfac80',
+        	           })
+		if response.status_code == 200:
+			results = response.json()
+			return JsonResponse(results)
+		else:
+			return HttpResponseBadRequest()
+				
+	except Exception as ex:
+		context = {"error":ex}
+		return render(request,'paystacktransapp/homepage.html',context)		
+    				
