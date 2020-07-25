3769a8da23cf3472b025d70e7cbd608103e1e027
jaymes15 <jamesoluwatomisinomotosho@gmail.com>
2020-07-25 07:55:33 +0400

    receipt generation and transfer payment

--- paystacktransapp/urls.py
+++ paystacktransapp/urls.py
@@ -7,6 +7,7 @@ from django.urls import path
 app_name= 'paystacktransapp'
 urlpatterns = [
 	path('',views.homepage, name='homepage'), 
-	path('checkaccount/',views.checkaccount,name="checkaccount")
+	path('checkaccount/',views.checkaccount,name="checkaccount"),
+	path('generate_receipt/',views.generate_receipt,name="generate_receipt")
 	
 	]
--- paystacktransapp/views.py
+++ paystacktransapp/views.py
@@ -28,7 +28,8 @@ def homepage(request):
 
 def checkaccount(request):
 	try:
-		print("hello")
+		
+		global bankcode
 		bankcode = request.POST.get('bankcode',None)
 		accountnumber = request.POST.get('accountnumber',None)
 		headers = {
@@ -39,11 +40,69 @@ def checkaccount(request):
         	           })
 		if response.status_code == 200:
 			results = response.json()
+			global acccount_details 
+			acccount_details = results
 			return JsonResponse(results)
 		else:
 			return HttpResponseBadRequest()
 				
 	except Exception as ex:
 		context = {"error":ex}
-		return render(request,'paystacktransapp/homepage.html',context)		
+		return render(request,'paystacktransapp/homepage.html',context)	
+
+
+
+
+def generate_receipt(request):
+	try:
+		amount = request.POST.get('amount',None)
+
+		he = {"Authorization": "Bearer sk_test_1590017dabc1772e81fd0e246379f6e6ebcfac80","Content-Type" : "application/json"}
+		info ={"type": "nuban",
+		        "name": str(acccount_details['data']['account_name']),
+				"account_number": str(acccount_details['data']['account_number']),
+				"bank_code": str(bankcode),
+				"currency": "NGN"
+				}
+		try:
+			response = requests.post("https://api.paystack.co/transferrecipient",json=info,headers=he)
+			if response.status_code == 201:
+					results = response.json()
+					global receipt
+					receipt = results
+					he = {"Authorization": "Bearer sk_test_1590017dabc1772e81fd0e246379f6e6ebcfac80","Content-Type" : "application/json"}
+					info ={ "source": "balance",
+							 "amount": int(amount),
+							 "recipient": str(receipt['data']['recipient_code']),
+							 "reason": "Holiday Flexing" 
+							 }
+							 
+					response = requests.post("https://api.paystack.co/transfer",json=info,headers=he)
+					if response.status_code == 201:
+								message={"message":"You have been credited"}
+								return JsonResponse(message)
+					else:
+							
+							message={"message":"You cannot initiate third party payouts as a starter business"}
+							return JsonResponse(message)		
+		except Exception as ex:
+			message={"message":ex}
+			return JsonResponse(message)		
+					
+	except Exception as ex:
+		message={"message":ex}
+		return JsonResponse(message)
+
+				
+
+
+
+
+
+
+
+
+
+
+
     				
